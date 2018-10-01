946f45530405965646e932353f8e39ecc9eb7827
Brett Zamir <brettz9@yahoo.com>
2018-10-01 15:34:07 +0800

    - Refactoring: Use ESM for `simple-post-json`, lbs, `async`/`await`, ellipsis, `ChildNode` - npm: Update `simple-post-json` - npm: Move scripts to top; add `opn-cli` for auto-opening URL

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 /* eslint-env browser */
-/* globals postJSON */
 
-// Todo: Provide ESM export for simple-post-json
+import postJSON from './node_modules/simple-post-json/dist/index-es.js';
 import {jml} from './node_modules/jamilih/dist/jml-es.js';
 
-const standardHeaders = ['Accept', 'Accept-Charset', 'Accept-Encoding', 'Accept-Language', 'Accept-Datetime', 'Authorization', 'Cache-Control', 'Connection', 'Connection: Upgrade', 'Permanent', 'Cookie', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'Expect', 'Forwarded', 'From', 'Host', 'If-Match', 'If-Modified-Since', 'If-None-Match', 'If-Range', 'If-Unmodified-Since', 'Max-Forwards', 'Origin', 'Pragma', 'Proxy-Authorization', 'Range', 'Referer', 'TE', 'User-Agent', 'Upgrade', 'Via', 'Warning'];
-const nonstandardHeaders = ['X-Requested-With', 'DNT', 'X-Forwarded-For', 'X-Forwarded-Host', 'X-Forwarded-Proto', 'Front-End-Https', 'X-Http-Method-Override', 'X-ATT-DeviceId', 'X-Wap-Profile', 'Proxy-Connection', 'X-UIDH', 'X-Csrf-Token', 'X-Request-ID', 'X-Correlation-ID'];
+const standardHeaders = [
+    'Accept', 'Accept-Charset', 'Accept-Encoding', 'Accept-Language',
+    'Accept-Datetime', 'Authorization', 'Cache-Control', 'Connection',
+    'Connection: Upgrade', 'Permanent', 'Cookie', 'Content-Length',
+    'Content-MD5', 'Content-Type', 'Date', 'Expect', 'Forwarded',
+    'From', 'Host', 'If-Match', 'If-Modified-Since', 'If-None-Match',
+    'If-Range', 'If-Unmodified-Since', 'Max-Forwards', 'Origin',
+    'Pragma', 'Proxy-Authorization', 'Range', 'Referer', 'TE',
+    'User-Agent', 'Upgrade', 'Via', 'Warning'
+];
+const nonstandardHeaders = [
+    'X-Requested-With', 'DNT', 'X-Forwarded-For', 'X-Forwarded-Host',
+    'X-Forwarded-Proto', 'Front-End-Https', 'X-Http-Method-Override',
+    'X-ATT-DeviceId', 'X-Wap-Profile', 'Proxy-Connection', 'X-UIDH',
+    'X-Csrf-Token', 'X-Request-ID', 'X-Correlation-ID'
+];
 
 function $ (sel) {
     return document.querySelector(sel);
 }
-function replace (oldNode, newNode) {
-    return oldNode.parentNode.replaceChild(newNode, oldNode);
-}
-function remove (node) {
-    return node.parentNode.removeChild(node);
-}
-function post (e) {
+
+async function post (e) {
     const url = e.target.value;
-    postJSON({
+    const res = await postJSON({
         url: 'retrieve.js',
         body: {
             url,
-            headers: Array.from($('table.requestHeaders').rows).slice(1).reduce((h, row) => {
+            headers: [...$('table.requestHeaders').rows].slice(1).reduce((h, row) => {
                 const cells = row.cells;
                 const headerName = cells[1].querySelector('input').value;
                 if (!headerName.trim()) {
@@ -32,31 +40,30 @@ function post (e) {
                 return h;
             }, {})
         }
-    }).then(function (res) {
-        const doc = new DOMParser().parseFromString(res.html, 'text/html');
-        if (!Array.from(doc.querySelectorAll('base')).some((base) => !!base.href)) {
-            doc.head.appendChild(jml('base', {href: url}));
-        }
-        replace($('#htmlPreview'), jml('iframe', {
-            id: 'htmlPreview',
-            className: 'htmlPreview',
-            srcdoc: doc.documentElement.outerHTML
-            // sandbox: 'allow-scripts'
-        }));
-        $('#htmlText').value = res.html;
-        replace($('#responseHeaders').firstElementChild, jml('table', {className: 'responseHeaders'}, [
-            ['tr', [
-                ['th', ['Header name']],
-                ['th', ['Value']]
-            ]],
-            ...Object.entries(res.headers).map(([name, val]) =>
-                ['tr', [
-                    ['td', [name]],
-                    ['td', [val]]
-                ]]
-            )
-        ]));
     });
+    const doc = new DOMParser().parseFromString(res.html, 'text/html');
+    if (!Array.from(doc.querySelectorAll('base')).some((base) => !!base.href)) {
+        doc.head.appendChild(jml('base', {href: url}));
+    }
+    $('#htmlPreview').replaceWith(jml('iframe', {
+        id: 'htmlPreview',
+        className: 'htmlPreview',
+        srcdoc: doc.documentElement.outerHTML
+        // sandbox: 'allow-scripts'
+    }));
+    $('#htmlText').value = res.html;
+    $('#responseHeaders').firstElementChild.replaceWith(jml('table', {className: 'responseHeaders'}, [
+        ['tr', [
+            ['th', ['Header name']],
+            ['th', ['Value']]
+        ]],
+        ...Object.entries(res.headers).map(([name, val]) =>
+            ['tr', [
+                ['td', [name]],
+                ['td', [val]]
+            ]]
+        )
+    ]));
 }
 
 function createRequestHeaderRow () {
@@ -69,7 +76,7 @@ function createRequestHeaderRow () {
             }}}, ['+']],
             ['button', {$on: {click: (e) => {
                 if ($('table.requestHeaders').rows.length > 2) {
-                    remove(e.target.parentNode.parentNode);
+                    e.target.parentNode.parentNode.remove();
                 }
             }}}, ['-']]
         ]],
